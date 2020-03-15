24a1645e0b821860c73feaf510cef34505b79a42
Wesley Haines <wesley.haines@me.com>
2020-03-15 17:51:29 -0400

    font and js changes

--- js/explore.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-	var explore = jQuery('#gsx').offset().top;
-
-	jQuery('#topbutton').click(function(){
-		jQuery('html, body').animate({scrollTop:explore}, 'slow');
-		return false;
-	});
-});
